b64741aec0c8a941a25bdbfa19db58d52cd6049b
vimfung <vimfung@qq.com>
2016-08-22 21:03:33 +0800

    no message

---
+++ Source/Android/Sample/luascriptcore/src/main/jni/LuaNativeContext.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vimfung on 16/8/22.
+//
+
+#include "LuaNativeContext.h"
---
+++ Source/Android/Sample/luascriptcore/src/main/jni/cn_vimfung_luascriptcore_LuaContext.cpp
@@ -0,0 +1,14 @@
+//
+// Created by vimfung on 16/8/22.
+//
+
+#include "cn_vimfung_luascriptcore_LuaContext.h"
+#include "LuaNativeContext.h"
+#include <map>
+
+
+
+JNIEXPORT void JNICALL Java_cn_vimfung_luascriptcore_LuaContext_createContext (JNIEnv * env, jobject obj, jstring name)
+{
+
+}
