1ec513991954804dd4d37d510db5c00c1be53f7f
vimfung <vimfung@qq.com>
2016-12-01 18:41:20 +0800

    增加在iOS下的Swift语言开发库

---
+++ Sample/iOS_OSX/Sample-OSX-Swift/LSCTPerson.swift
@@ -0,0 +1,25 @@
+//
+//  LSCTPerson.swift
+//  Sample
+//
+//  Created by 冯鸿杰 on 16/12/1.
+//  Copyright © 2016年 vimfung. All rights reserved.
+//
+
+import LuaScriptCore_OSX_Swift
+
+class LSCTPerson: LSCObjectClass
+{
+    var name : String? = nil;
+    
+    func speak() -> Void
+    {
+        NSLog("%@ speak", name ?? "noname");
+    }
+    
+    func walk() -> Void
+    {
+        NSLog("%@ walk", name ?? "noname");
+    }
+    
+}
