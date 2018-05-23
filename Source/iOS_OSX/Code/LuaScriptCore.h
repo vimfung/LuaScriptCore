e70901b38d337d55127b43fc1e41788539ed2ec1
vimfung <vimfung@qq.com>
2018-05-23 11:06:39 +0800

    调整版本号，定版2.2.1

--- Source/iOS_OSX/Code/LuaScriptCore.h
+++ Source/iOS_OSX/Code/LuaScriptCore.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 vimfung. All rights reserved.
 //
 
-#define LuaScriptCoreVersion @"2.1.1"
+#define LuaScriptCoreVersion @"2.2.1"
 
 #import "LSCContext.h"
 #import "LSCValue.h"
