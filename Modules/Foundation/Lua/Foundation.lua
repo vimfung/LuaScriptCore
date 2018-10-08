7302fecd62abc5e471bf3cb7c542d6d106fc64e5
vimfung <vimfung@qq.com>
2018-10-08 09:21:54 +0800

    分离Modules目录，迁移到LuaScriptCore-Modules项目中独立维护

--- Modules/Foundation/Lua/Foundation.lua
+++
@@ -1,5 +0,0 @@
-Object:typeMapping("ios", "LSCEncoding", "Encoding");
-Object:typeMapping("ios", "LSCPath", "Path");
-
-Object:typeMapping("android", "cn.vimfung.luascriptcore.modules.foundation.Encoding", "Encoding");
-Object:typeMapping("android", "cn.vimfung.luascriptcore.modules.foundation.Path", "Path");
